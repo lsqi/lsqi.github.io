36b459a32ef340684f41744d44aaac7a302952e7
lsqi <1966055708@qq.com>
2020-08-17 22:08:35 +0800

    Site updated: 2020-08-17 22:08:34

--- sw.js
+++ sw.js
@@ -28,17 +28,17 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/59524.html');
 
-  precacheUrls.push('/posts/42500.html');
+  precacheUrls.push('/posts/64520.html');
 
   precacheUrls.push('/posts/46268.html');
 
-  precacheUrls.push('/posts/64520.html');
+  precacheUrls.push('/posts/42500.html');
 
-  precacheUrls.push('/posts/45299.html');
+  precacheUrls.push('/posts/14066.html');
 
   precacheUrls.push('/posts/63379.html');
 
-  precacheUrls.push('/posts/14066.html');
+  precacheUrls.push('/posts/45299.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
